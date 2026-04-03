8ec0f14250f04cde6b5460060eb2989c09f01a66
phippsammon <phippsammon175@gmail.com>
2026-04-03 15:00:07 -0600

    Fixed image

--- Assignments/Assignment6/visualize.py
+++ Assignments/Assignment6/visualize.py
@@ -10,15 +10,15 @@ files = {
 
 for filename, title in files.items():
     try:
-        binary = np.fromfile(filename, dtype = np.int32).reshape((N, N))
+        binary = np.fromfile(filename, dtype=np.int32).reshape((N, N))
         out_png = filename.replace(".raw", ".png")
-        plt.figure(figsize=(6, 6))
-        plt.imshow(binary, cmap='gray', vmin=0, vmax=1)
-        plt.title(title)
-        plt.axis('off')
-        plt.savefig(out_png, bbox_inches='tight', pad_inches=0, dpi = 150)
+        fig, ax = plt.subplots(figsize=(6, 6))
+        ax.imshow(binary, cmap='gray', vmin=0, vmax=1)
+        ax.axis('off')
+        fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
+        fig.savefig(out_png, dpi=150, facecolor='black')
         plt.close()
         alive = binary.sum()
         print(f"{filename} -> {out_png} | Alive Cells: {alive}")
     except Exception as e:
-        print (f"Error processing {filename}: {e}")
+        print(f"Error processing {filename}: {e}")
